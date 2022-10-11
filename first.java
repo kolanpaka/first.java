d97a6a7ce4568548a165aeda213a6ef1c8cb7a73
Nithin984 <nithinvanga01@gmail.com>
2022-10-11 21:04:35 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,17 @@
+
+
+public class first{
+	
+  public static void main(String a[])
+  
+  {
+  System.out.print("nithin vangabvfgvfudg");
+    System.out.println("nithin vanga");
+
+		System.out.println("nithin vanga");
+		
+  }
+  
+
+
+  }
